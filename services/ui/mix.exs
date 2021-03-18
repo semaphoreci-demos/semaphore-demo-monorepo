2eb796956870a2de3bb493445b5bbc41421efeff
Igor Šarčević <igor@renderedtext.com>
2021-03-18 15:53:22 +0100

    Run credo to lint elixir

--- services/ui/mix.exs
+++ services/ui/mix.exs
@@ -22,7 +22,8 @@ defmodule Ui.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false}
     ]
   end
 end
