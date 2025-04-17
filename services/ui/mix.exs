97d8f4174f747d59d8639635bd147722d393fe51
tomas f <46322567+TomFern@users.noreply.github.com>
2025-04-17 12:12:12 +0100

    elixir tests

--- services/ui/mix.exs
+++ services/ui/mix.exs
@@ -5,7 +5,7 @@ defmodule Ui.MixProject do
     [
       app: :ui,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
